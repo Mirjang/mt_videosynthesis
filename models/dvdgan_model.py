d5dcc338b6ce48a967c3c69e1e1bce0331e923dd
patrickradner_korhal <radner.patrick@gmail.com>
2020-09-23 23:24:37 +0200

    river_relaxing_dvdgan_style_128_0

--- models/dvdgan_model.py
+++ models/dvdgan_model.py
@@ -246,6 +246,8 @@ class DvdStyleConditionalGenerator(nn.Module):
             y = noise.view(encoder_list[0].shape) # B x ch x ld x ld
         else: #use encoded frame
             y = encoder_list[0] # B x ch x ld x ld
+        
+        print(y.shape, self.encoder2style(y).shape, self.encoder2style(y).unsqueeze(1).expand(-1, self.nframes, -1).shape, x.size(0)*self.nframes)
         style = self.encoder2style(y).unsqueeze(1).expand(-1, self.nframes, -1).view(x.size(0)*self.nframes, -1) # BT x style
 
         depth = 0
