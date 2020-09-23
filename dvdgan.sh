dca7d298a158eae31b32b1cc73b32bafe0d1eb78
patrickradner_korhal <radner.patrick@gmail.com>
2020-09-23 22:34:04 +0200

    trying tpu again

--- dvdgan.sh
+++ dvdgan.sh
@@ -45,7 +45,7 @@ DISP_FRAMES=16
 VAL_SIZE=100
 VAL_SET=val #split
 #--verbose --sanity_check
-python train.py --continue_train --ch_g 32 --ch_ds 16 --ch_dt 16 --conditional --lr_policy cosine --niter 10 --niter_decay 250 --clip_grads .1 --n_critic 3 --lambda_L1 50 --lambda_S 1 --lambda_T 10 --lambda_AUX 100 --max_clip_length $LEN --skip_frames $SKIP --validation_freq 5 --validation_set $VAL_SET --pretrain_epochs 0 --tlg .3 --tld .7 --max_val_dataset_size $VAL_SIZE --init_type orthogonal --batch_size $BATCHSIZE --parallell_batch_size $SUB_BATCH --resolution $RESOLUTION --fps $FPS --dataroot $DATASETS_DIR/$DATASET --checkpoints_dir $CHECKPOINT_DIR --name $NAME --model $MODEL --generator $GENERATOR --dataset_mode $DATASET_MODE --gpu_ids $GPU_ID --lr $LR --display_port $VISDOM_PORT --display_env $DISPNAME --update_html_freq $FREQ --num_display_frames $DISP_FRAMES --print_freq $FREQ --display_freq $FREQ
+python train.py --ch_g 32 --ch_ds 16 --ch_dt 16 --conditional --lr_policy cosine --niter 10 --niter_decay 250 --clip_grads .1 --n_critic 3 --lambda_L1 50 --lambda_S 1 --lambda_T 10 --lambda_AUX 100 --max_clip_length $LEN --skip_frames $SKIP --validation_freq 5 --validation_set $VAL_SET --pretrain_epochs 0 --tlg .3 --tld .7 --max_val_dataset_size $VAL_SIZE --init_type orthogonal --batch_size $BATCHSIZE --parallell_batch_size $SUB_BATCH --resolution $RESOLUTION --fps $FPS --dataroot $DATASETS_DIR/$DATASET --checkpoints_dir $CHECKPOINT_DIR --name $NAME --model $MODEL --generator $GENERATOR --dataset_mode $DATASET_MODE --gpu_ids $GPU_ID --lr $LR --display_port $VISDOM_PORT --display_env $DISPNAME --update_html_freq $FREQ --num_display_frames $DISP_FRAMES --print_freq $FREQ --display_freq $FREQ
 
 DATASET=gaugan
 DATASET_MODE=image
