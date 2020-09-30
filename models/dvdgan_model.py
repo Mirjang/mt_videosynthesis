ffed1d5c7df4f488be8490940c58a4b706582401
patrickradner_korhal <radner.patrick@gmail.com>
2020-10-01 00:05:34 +0200

    river_relaxing_dvdgan_dvdgan3d_128_noise_cgan_noaux_seg_g16s16t32_noprepool

--- models/dvdgan_model.py
+++ models/dvdgan_model.py
@@ -198,8 +198,8 @@ class Dvd3DConditionalGenerator(nn.Module):
             c = CH[d]
             rnn.append(ConvGRU(c * ch, hidden_sizes=[c * ch], kernel_sizes=3, n_layers=n_layers),)
             conv.append(nn.Sequential(
-                GResBlock3D(c, c, n_class=1, upsample_factor=2, bn = bn, norm = norm),
-                GResBlock3D(c, CH[d+1], n_class=1, upsample_factor=1, bn = bn, norm = norm),
+                GResBlock3D(ch*c, ch*c, n_class=1, upsample_factor=2, bn = bn, norm = norm),
+                GResBlock3D(ch*c, ch*CH[d+1], n_class=1, upsample_factor=1, bn = bn, norm = norm),
             ))
         self.rnn = nn.ModuleList(rnn)
         self.conv = nn.ModuleList(conv)
