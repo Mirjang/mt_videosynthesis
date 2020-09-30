27d29d9380fae4796c804c33ce46a6ad0dac8d60
patrickradner_korhal <radner.patrick@gmail.com>
2020-09-30 23:40:07 +0200

    river_relaxing_dvdgan_dvdgan3d_128_noise_cgan_noaux_seg_g16s16t32_noprepool

--- models/dvdgan_model.py
+++ models/dvdgan_model.py
@@ -231,6 +231,7 @@ class Dvd3DConditionalGenerator(nn.Module):
             print(f"----Depth: {depth}----")
             unrolls = math.ceil(self.nframes / (2**depth))
             frame_list = [encoder_list[depth]]
+            print(encoder_list[depth].shape)
             for i in range(unrolls): 
                 print(f"unroll {i}/{unrolls}")
 
