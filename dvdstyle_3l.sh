83356d0ea73f042223144727625570dfe8523715
patrickradner_korhal <radner.patrick@gmail.com>
2020-10-04 20:44:04 +0200

    river_relaxing_dvdgan_style_256_

--- dvdstyle_3l.sh
+++ dvdstyle_3l.sh
@@ -1,7 +1,7 @@
 set -ex
 
 # GPU
-GPU_ID=0
+GPU_ID=1
 
 if [[ $(nvidia-smi | grep "^|    $GPU_ID    ") ]]; then
     read -p "GPU currently in use, continue? " -n 1 -r
@@ -30,11 +30,11 @@ MODEL=dvdgan
 LR=0.0003
 BATCHSIZE=32
 SUB_BATCH=1
-RESOLUTION=256
+RESOLUTION=128
 FPS=25
 GENERATOR=style
 
-NAME=${DATASET}_${MODEL}_${GENERATOR}_${RESOLUTION}_
+NAME=${DATASET}_${MODEL}_${GENERATOR}_${RESOLUTION}_3grulayers
 git add -A 
 git commit -m $NAME
 DISPNAME=${NAME}
@@ -47,7 +47,7 @@ DISP_FRAMES=16
 VAL_SIZE=100
 VAL_SET=val #split
 #--verbose --sanity_check
-python train.py --ch_g 32 --ch_ds 16 --ch_dt 64 --no_dt_prepool --lr_policy cosine --gru_layers 2 --style_noise --niter 10 --niter_decay 250 --clip_grads 2.5 --n_critic 3 --lambda_S 1 --lambda_T 1 --max_clip_length $LEN --skip_frames $SKIP --validation_freq 5 --validation_set $VAL_SET --max_val_dataset_size $VAL_SIZE --init_type orthogonal --batch_size $BATCHSIZE --parallell_batch_size $SUB_BATCH --resolution $RESOLUTION --fps $FPS --dataroot $DATASETS_DIR/$DATASET --checkpoints_dir $CHECKPOINT_DIR --name $NAME --model $MODEL --generator $GENERATOR --dataset_mode $DATASET_MODE --gpu_ids $GPU_ID --lr $LR --display_port $VISDOM_PORT --display_env $DISPNAME --update_html_freq $FREQ --num_display_frames $DISP_FRAMES --print_freq $FREQ --display_freq $FREQ
+python train.py --ch_g 32 --ch_ds 16 --ch_dt 64 --no_dt_prepool --lr_policy cosine --gru_layers 3 --style_noise --niter 10 --niter_decay 250 --clip_grads 2.5 --n_critic 3 --lambda_S 1 --lambda_T 1 --max_clip_length $LEN --skip_frames $SKIP --validation_freq 5 --validation_set $VAL_SET --max_val_dataset_size $VAL_SIZE --init_type orthogonal --batch_size $BATCHSIZE --parallell_batch_size $SUB_BATCH --resolution $RESOLUTION --fps $FPS --dataroot $DATASETS_DIR/$DATASET --checkpoints_dir $CHECKPOINT_DIR --name $NAME --model $MODEL --generator $GENERATOR --dataset_mode $DATASET_MODE --gpu_ids $GPU_ID --lr $LR --display_port $VISDOM_PORT --display_env $DISPNAME --update_html_freq $FREQ --num_display_frames $DISP_FRAMES --print_freq $FREQ --display_freq $FREQ
 
 DATASET=gaugan
 DATASET_MODE=image
