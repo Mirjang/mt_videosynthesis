ba8bbedc6f0ec2e994919b80839c9f1d2d036a08
patrickradner_korhal <radner.patrick@gmail.com>
2020-10-05 00:48:01 +0200

    river_relaxing_dvdgan_style2_256

--- dvdstyle2.sh
+++ dvdstyle2.sh
@@ -47,7 +47,7 @@ DISP_FRAMES=16
 VAL_SIZE=100
 VAL_SET=val #split
 #--verbose --sanity_check
-python train.py --ch_g 8 --ch_ds 8 --ch_dt 16 --lr_policy cosine --gru_layers 1 --style_noise --niter 10 --niter_decay 250 --clip_grads 2.5 --n_critic 3 --lambda_S 1 --lambda_T 1 --max_clip_length $LEN --skip_frames $SKIP --validation_freq 5 --validation_set $VAL_SET --max_val_dataset_size $VAL_SIZE --init_type orthogonal --batch_size $BATCHSIZE --parallell_batch_size $SUB_BATCH --resolution $RESOLUTION --fps $FPS --dataroot $DATASETS_DIR/$DATASET --checkpoints_dir $CHECKPOINT_DIR --name $NAME --model $MODEL --generator $GENERATOR --dataset_mode $DATASET_MODE --gpu_ids $GPU_ID --lr $LR --display_port $VISDOM_PORT --display_env $DISPNAME --update_html_freq $FREQ --num_display_frames $DISP_FRAMES --print_freq $FREQ --display_freq $FREQ
+python train.py --ch_g 8 --ch_ds 8 --ch_dt 8 --lr_policy cosine --gru_layers 1 --style_noise --niter 10 --niter_decay 250 --clip_grads 2.5 --n_critic 3 --lambda_S 1 --lambda_T 1 --max_clip_length $LEN --skip_frames $SKIP --validation_freq 5 --validation_set $VAL_SET --max_val_dataset_size $VAL_SIZE --init_type orthogonal --batch_size $BATCHSIZE --parallell_batch_size $SUB_BATCH --resolution $RESOLUTION --fps $FPS --dataroot $DATASETS_DIR/$DATASET --checkpoints_dir $CHECKPOINT_DIR --name $NAME --model $MODEL --generator $GENERATOR --dataset_mode $DATASET_MODE --gpu_ids $GPU_ID --lr $LR --display_port $VISDOM_PORT --display_env $DISPNAME --update_html_freq $FREQ --num_display_frames $DISP_FRAMES --print_freq $FREQ --display_freq $FREQ
 
 DATASET=gaugan
 DATASET_MODE=image
