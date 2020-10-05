556319305bbb1596442ea022778057e2640b6789
patrickradner_korhal <radner.patrick@gmail.com>
2020-10-05 11:30:04 +0200

    river_relaxing_dvdgan_style_256_

--- dvdstyle2.sh
+++ dvdstyle2.sh
@@ -29,18 +29,18 @@ MODEL=dvdgan
 # optimizer parameters
 LR=0.0003
 BATCHSIZE=32
-SUB_BATCH=1
-RESOLUTION=256
+SUB_BATCH=2
+RESOLUTION=128
 FPS=25
 GENERATOR=style2
 
 NAME=${DATASET}_${MODEL}_${GENERATOR}_${RESOLUTION}
-git add -A 
-git status | grep modified
-if [ $? -eq 0 ]
-then
-    git commit -m $NAME
-fi
+# git add -A 
+# git status | grep modified
+# if [ $? -eq 0 ]
+# then
+#     git commit -m $NAME
+# fi
 DISPNAME=${NAME}
 # vid settings
 SKIP=1
@@ -51,7 +51,7 @@ DISP_FRAMES=16
 VAL_SIZE=100
 VAL_SET=val #split
 #--verbose --sanity_check
-python train.py --ch_g 8 --ch_ds 8 --ch_dt 16 --start_fp 3 --max_fp 10 --up_blocks_per_rnn 2 --lr_policy cosine --gru_layers 1 --style_noise --niter 10 --niter_decay 250 --clip_grads 2.5 --n_critic 3 --lambda_S 1 --lambda_T 1 --max_clip_length $LEN --skip_frames $SKIP --validation_freq 5 --validation_set $VAL_SET --max_val_dataset_size $VAL_SIZE --init_type orthogonal --batch_size $BATCHSIZE --parallell_batch_size $SUB_BATCH --resolution $RESOLUTION --fps $FPS --dataroot $DATASETS_DIR/$DATASET --checkpoints_dir $CHECKPOINT_DIR --name $NAME --model $MODEL --generator $GENERATOR --dataset_mode $DATASET_MODE --gpu_ids $GPU_ID --lr $LR --display_port $VISDOM_PORT --display_env $DISPNAME --update_html_freq $FREQ --num_display_frames $DISP_FRAMES --print_freq $FREQ --display_freq $FREQ
+python train.py --ch_g 32 --ch_ds 8 --no_dt_prepool --ch_dt 32 --start_fp 2 --max_fp 5 --up_blocks_per_rnn 2 --lr_policy cosine --gru_layers 1 --style_noise --niter 10 --niter_decay 250 --clip_grads 2.5 --n_critic 3 --lambda_S 1 --lambda_T 1 --max_clip_length $LEN --skip_frames $SKIP --validation_freq 5 --validation_set $VAL_SET --max_val_dataset_size $VAL_SIZE --init_type orthogonal --batch_size $BATCHSIZE --parallell_batch_size $SUB_BATCH --resolution $RESOLUTION --fps $FPS --dataroot $DATASETS_DIR/$DATASET --checkpoints_dir $CHECKPOINT_DIR --name $NAME --model $MODEL --generator $GENERATOR --dataset_mode $DATASET_MODE --gpu_ids $GPU_ID --lr $LR --display_port $VISDOM_PORT --display_env $DISPNAME --update_html_freq $FREQ --num_display_frames $DISP_FRAMES --print_freq $FREQ --display_freq $FREQ
 
 DATASET=gaugan
 DATASET_MODE=image
